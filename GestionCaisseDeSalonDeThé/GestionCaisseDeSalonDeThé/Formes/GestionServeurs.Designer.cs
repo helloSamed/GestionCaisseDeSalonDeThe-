a054567130c53413f3ac0514541ed36916c4ba24
Mohammed Abdessamad LAHROURI <lahrouri.abdessamad@gmail.com>
2022-04-24 17:00:33 +0000

    Update GestionServeurs.Designer.cs

--- GestionCaisseDeSalonDeThé/GestionCaisseDeSalonDeThé/Formes/GestionServeurs.Designer.cs
+++ GestionCaisseDeSalonDeThé/GestionCaisseDeSalonDeThé/Formes/GestionServeurs.Designer.cs
@@ -98,8 +98,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(760, 538);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
+            //this.Controls.Add(this.button5);
+            //this.Controls.Add(this.button4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
